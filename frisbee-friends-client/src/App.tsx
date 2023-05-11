e6f806290ea85cede74a9f78a4b9d2e83ccd1aca
jd <jdsanchez2011@gmail.com>
2023-05-10 23:38:36 -0600

    chore: fix bad import

--- frisbee-friends-client/src/App.tsx
+++ frisbee-friends-client/src/App.tsx
@@ -1,4 +1,3 @@
-import './App.css';
 import LandingPage from './LandingPage';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
